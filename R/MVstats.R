a9af8b78205c721f5d66e072fc40e2a331bac8ce
chrisaberson <cla18@humboldt.edu>
2020-04-13 15:51:18 -0700

    Add files via upload

---
+++ R/MVstats.R
@@ -0,0 +1,13 @@
+#' Tutorials for Multivariate Statistics
+#'
+#' @description
+#'
+#' This package supports online teaching for various multivariate statistics procedures
+#'
+#' @import lmtest
+#' @docType package
+#' @name MVstats
+#'
+#'
+#'
+NULL
